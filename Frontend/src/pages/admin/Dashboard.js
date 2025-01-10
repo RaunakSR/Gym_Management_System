3964e22900bb489992741fec3366d794579bf01b
RaunakSR <raunaksinghrathour@gmail.com>
2025-01-10 21:48:04 +0530

    Add files via upload

---
+++ Frontend/src/pages/admin/Dashboard.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Button } from "@mantine/core";
+import { Link } from "react-router-dom";
+
+const Dashboard = () => {
+	return (
+		<div>
+			<h1>Admin Dashboard</h1>
+
+			<Button>
+				<Link style={{ textDecoration: "none", color: "#fff" }} to="/workout-report">
+					Generate Workout Report
+				</Link>
+			</Button>
+			<br />
+			<br />
+			<br />
+			<Button style={{ margin: 20 }}>
+				<Link style={{ textDecoration: "none", color: "#fff" }} to="/salary">
+					Salary
+				</Link>
+			</Button>
+			<br />
+			<br />
+			<br />
+			<Button style={{ margin: 20 }}>
+				<Link style={{ textDecoration: "none", color: "#fff" }} to="/employeeList">
+					Employees
+				</Link>
+			</Button>
+			<br />
+			<br />
+			<br />
+			<Button>
+				<Link style={{ textDecoration: "none", color: "#fff" }} to="/workoutProgram-report">
+					Generate Workout Program Report
+				</Link>
+			</Button>
+			<br />
+			<br />
+			<br />
+			<Button>
+				<Link style={{ textDecoration: "none", color: "#fff" }} to="/reqList">
+					Set Personal Trainer Request Status
+				</Link>
+			</Button>
+			<br />
+			<br />
+			<br />
+			<Button>
+				<Link style={{ textDecoration: "none", color: "#fff" }} to="/pReport">
+					Generate Personal Trainer Request Report
+				</Link>
+			</Button>
+		</div>
+	);
+};
+
+export default Dashboard;
